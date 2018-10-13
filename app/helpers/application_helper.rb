cfdc2170f1ba34582abf300072aebe836355e2c7
Matt McGinnis <mcginnisma90@gmail.com>
2018-10-12 20:42:06 -0400

    Added some styling and a user avatar helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def user_avatar_helper(user, size, style_class)
+        if user.avatar.blank?
+            image_tag "Default-Avatar", size: size, class: style_class
+        else
+            image_tag user.avatar.url, size: size, class: style_class
+        end
+    end
 end
